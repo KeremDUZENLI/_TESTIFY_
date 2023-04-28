0df482d35f592c67ee2bc789002a0b0ab9432958
kerem.duzenli <krmdznl97@gmail.com>
2023-04-28 19:08:44 -0300

    build: used suite.Suite

--- calculation/priceIncrease_test.go
+++ calculation/priceIncrease_test.go
@@ -3,33 +3,90 @@ package calculation
 import (
 	"database/sql"
 	"testify/database"
+	"testify/helper"
 	"testify/model"
 	"testing"
 
 	_ "github.com/lib/pq"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func Test_PriceIncrease(t *testing.T) {
-	requires := require.New(t)
+type IntTestSuite struct {
+	suite.Suite
+	db            *sql.DB
+	priceIncrease PriceIncrease
+}
 
+func TestIntTestSuite(t *testing.T) {
+	suite.Run(t, &IntTestSuite{})
+}
+
+func (its *IntTestSuite) SetupSuite() {
 	db := database.DbConnect()
-	dbSetup(t, db)
+
+	dbCreateTableTest(db)
+	database.DbCreateTable(db)
 
 	mp := model.NewPriceProvider(db)
 	priceIncrease := NewPriceIncrease(mp)
-	percentage, err := priceIncrease.PriceIncrease()
 
-	requires.Nil(err)
-	requires.Equal(25.0, percentage)
+	its.db = db
+	its.priceIncrease = priceIncrease
 }
 
-func dbSetup(t *testing.T, db *sql.DB) {
-	_, err := db.Exec(`CREATE DATABASE IF NOT EXISTS stockprices_test`)
-	if err != nil {
-		t.Logf(err.Error())
+func (its *IntTestSuite) BeforeTest(suiteName, testName string) {
+	if testName == "TestPriceIncrease_Error" {
+		return
 	}
 
-	database.DbCreateTable(db)
-	database.DbSeedTable(db)
+	database.DbSeedTable(its.db)
+}
+
+func (its *IntTestSuite) TearDownSuite() {
+	tearDownDatabase(its)
+}
+
+func (its *IntTestSuite) TearDownTest() {
+	cleanTable(its)
+}
+
+func (its *IntTestSuite) TestPriceIncrease_Error() {
+	percentage, err := its.priceIncrease.PriceIncrease()
+
+	its.EqualError(err, "not enough data")
+	its.Equal(0.0, percentage)
+}
+
+func (its *IntTestSuite) TestPriceIncrease() {
+	percentage, err := its.priceIncrease.PriceIncrease()
+
+	its.Nil(err)
+	its.Equal(25.0, percentage)
+}
+
+func dbCreateTableTest(db *sql.DB) {
+	println("setting up database")
+
+	_, err := db.Exec(`CREATE DATABASE stockprices_test`)
+	helper.ErrorIts(err)
+}
+
+func cleanTable(its *IntTestSuite) {
+	println("cleaning up table")
+
+	_, err := its.db.Exec(`DELETE FROM stockprices`)
+	helper.ErrorIts(err)
+}
+
+func tearDownDatabase(its *IntTestSuite) {
+	println("drop table & database")
+
+	_, err := its.db.Exec(`DROP TABLE stockprices`)
+	helper.ErrorIts(err)
+
+	_, err = its.db.Exec(`DROP DATABASE stockprices_test`)
+	helper.ErrorIts(err)
+
+	err = its.db.Close()
+	helper.ErrorIts(err)
 }
--- helper/helper.go
+++ helper/helper.go
@@ -2,10 +2,19 @@ package helper
 
 import (
 	"log"
+
+	"github.com/stretchr/testify/suite"
 )
 
 func ErrorLog(err error) {
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func ErrorIts(err error) {
+	var its suite.Suite
+	if err != nil {
+		its.FailNow(err.Error())
 	}
 }
